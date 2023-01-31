54a48d6941bc778954ebb5c02fd69f04d2884b9a
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-30 22:44:47 -0800

    Add logic to function checking if a number is divisible by 10.

--- divisible-by.js
+++ divisible-by.js
@@ -1,5 +1,10 @@
 /* Create a function that returns a Boolean if a number is divisible by 10 */
 
 const checkIfDivisibleBy = function(number) {
-
+  if (number % 10 !== 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
 };
