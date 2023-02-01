f648ef9ded0955cc775d00bfa0d7edcdcf41b8ee
Raiza De Guzman <santos_dg7@hotmail.com>
2023-01-31 21:53:24 -0800

    Change loop type to traditional for loop to resolve error of number not being iterable.

--- even-numbers.js
+++ even-numbers.js
@@ -1,7 +1,7 @@
 // Print all even numbers from 0 to 10
 
 const evenNumbers = function(number) {
-  for (const num of number) {
+  for (let num = 0; num <= number; num++) {
     if (num % 2 === 0) {
       console.log(num);
     }
