6595c2ef7575b1f81533bbb5523dab54d5c7e6ba
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-01 21:21:21 -0800

    Edit challenge instructions. Change function's parameter to take in an array.

--- filter-negnums.js
+++ filter-negnums.js
@@ -1,6 +1,6 @@
-// Create a function that filters out negative numbers
+// Create a function that takes in an array of numbers and filters out any negative numbers in the list.
 
-const filterNegNumbers = function(number) {
+const filterNegNumbers = function(array) {
   if (number < 0) {
     
   }
