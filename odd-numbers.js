58204f098bdbcab73465af1a3fee045692f1aef6
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-04 22:59:17 -0800

    Add logic to iterate over each number from 0 to 100.

--- odd-numbers.js
+++ odd-numbers.js
@@ -1,5 +1,9 @@
 // Print all numbers from 0 - 100
 
 const oddNumbers = function(limit) {
-
+  for (let num = 0; num < 100; num++) {
+    if (num % 2 !== 0) {
+      console.log(num);
+    }
+  }
 };
