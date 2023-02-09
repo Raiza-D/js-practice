7888f1262371ce83a05e75ade83328ad23f0a2be
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-08 22:42:49 -0800

    Declare let variable to keep track of tally. Write for of loop to iterate array values. Return tally variable.

--- sum-arrays.js
+++ sum-arrays.js
@@ -1,1 +1,12 @@
 /* Sum the numbers in a list. Assume all the items are numbers already. */
+
+const sumNumbers = function(array) {
+  let sum = 0;
+  for (const elem of array) {
+    sum += elem;
+  }
+
+  return sum;
+}
+
+console.log(sumNumbers([1, 2, 3, 4, 5]));
