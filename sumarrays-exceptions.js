5c381c78e41586b2fc5debf146579d3f4eca4d59
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-09 22:44:30 -0800

    Write declare sum variable to keep track of added values. Write logic to iterate over array elements and returns sum.

--- sumarrays-exceptions.js
+++ sumarrays-exceptions.js
@@ -1,5 +1,13 @@
 /* Sum the numbers in a list. The list may contain non-number type values. */
 
 const sumArraysExceptions = function(array) {
+  let sum = 0;
 
+  for (const elem of array) {
+    if (typeof elem === "number") {
+      sum += elem;
+    }
+  }
+
+  return sum;
 };
