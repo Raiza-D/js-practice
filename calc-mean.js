9a521c24fb7e72cb4cd0a7a83087a0ca64236c6a
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-13 11:21:59 -0800

    Write logic to sum values and divide by array's length.

--- calc-mean.js
+++ calc-mean.js
@@ -1,9 +1,15 @@
 // Calculate the MEAN -- average value of numbers in a list.
 
 const mean = function(array) {
+  let sum = 0;
 
-  
+  for (const elem of array) {
+    sum += sum;
+  }
 
+  let avg = sum / array.length;
+
+  return avg;
 };
 
 console.log(mean[4, 3, 9, 4, 5]);
