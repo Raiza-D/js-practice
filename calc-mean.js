48d9b4bcf58e30ee03847dd6633c783a6f68f6b4
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-13 11:25:13 -0800

    Fix undefined output by adding console log within for loop and wrapping array parameter in parentheses.

--- calc-mean.js
+++ calc-mean.js
@@ -4,7 +4,8 @@ const mean = function(array) {
   let sum = 0;
 
   for (const elem of array) {
-    sum += sum;
+    sum += elem;
+    console.log(sum);
   }
 
   let avg = sum / array.length;
@@ -12,4 +13,4 @@ const mean = function(array) {
   return avg;
 };
 
-console.log(mean[4, 3, 9, 4, 5]);
+console.log(mean([4, 3, 9, 4, 5]));
