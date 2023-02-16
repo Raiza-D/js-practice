5e30c0cfb529662caf2786ea2b7812256dfb923e
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-15 17:11:37 -0800

    Remove Math round method as it rounds to whole integer only.

--- km-m-conversion.js
+++ km-m-conversion.js
@@ -1,7 +1,7 @@
 // Write a function that converts kilometres to miles. Round to 2 decimal places.
 
 const unitConverter = function(kmValue) {
-  let miles = Math.round(kmValue * 0.62137119);
+  let miles = kmValue * 0.62137119;
 
   return miles;
 };
