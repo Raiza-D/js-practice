64ed761c1de22ed4d050e8f8a1731d6afe97d6fc
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-15 17:10:49 -0800

    Change multiplication value. Previous value was wrong.

--- km-m-conversion.js
+++ km-m-conversion.js
@@ -1,7 +1,7 @@
 // Write a function that converts kilometres to miles. Round to 2 decimal places.
 
 const unitConverter = function(kmValue) {
-  let miles = Math.round(kmValue * 1000)
+  let miles = Math.round(kmValue * 0.62137119);
 
   return miles;
 };
