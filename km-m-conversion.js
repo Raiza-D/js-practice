7fb3977409a84f3b15b9266d372205227977d360
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-15 16:58:03 -0800

    Write formula to convert km to m. Use Math round method.

--- km-m-conversion.js
+++ km-m-conversion.js
@@ -1,5 +1,7 @@
 // Write a function that converts kilometres to miles. Round to 2 decimal places.
 
-const unitConverter = function() {
+const unitConverter = function(kmValue) {
+  let miles = Math.round(kmValue * 1000)
 
+  return miles;
 };
