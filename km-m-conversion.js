8ece3a2c1120306047e376eb060371d61fdacde5
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-15 17:16:41 -0800

    Use toFixed method to round to two decimal places. Use Number function to convert rounded value to a number.

--- km-m-conversion.js
+++ km-m-conversion.js
@@ -1,9 +1,9 @@
 // Write a function that converts kilometres to miles. Round to 2 decimal places.
 
 const unitConverter = function(kmValue) {
-  let miles = kmValue * 0.62137119;
+  let miles = (kmValue * 0.62137119).toFixed(2);
 
-  return miles;
+  return Number(miles);
 };
 
 console.log(unitConverter(145));
