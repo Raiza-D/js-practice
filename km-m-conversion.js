1028fcd6ffb024e23ca810433dc74dc2156ae390
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-15 17:19:17 -0800

    Add comments

--- km-m-conversion.js
+++ km-m-conversion.js
@@ -1,8 +1,11 @@
 // Write a function that converts kilometres to miles. Round to 2 decimal places.
 
 const unitConverter = function(kmValue) {
+  // Use toFixed method to round calculated value to two decimal places.
+  // toFixed outputs a string
   let miles = (kmValue * 0.62137119).toFixed(2);
 
+  // Use Number function to convert calculated value back to a Number.
   return Number(miles);
 };
 
