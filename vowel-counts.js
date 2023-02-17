368265d3830a9bc92cfb1b1aa58a8993e8835878
Raiza De Guzman <santos_dg7@hotmail.com>
2023-02-16 20:10:48 -0800

    Declare counter variable. Add for of loop and return statement as part of logic to count vowels.

--- vowel-counts.js
+++ vowel-counts.js
@@ -1,11 +1,16 @@
 // Create a function that returns the number of vowels in a string
 
 const vowelCount = function(words) {
-
   const vowels = ["a", "e", "i", "o", "u"];
+  let vowelCount = 0;
 
+  for (const letter of words) {
+    if (vowels.includes(letter)) {
+      vowelCount++;
+    }
+  }
 
-
+  return vowelCount;
 };
 
 console.log(vowelCount("Crocodile Dundee"));
